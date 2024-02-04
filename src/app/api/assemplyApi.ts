14acdd8653bab6cb64abb46fa1bbd0a291f5fb54
hunman89 <hunman89@gmail.com>
2024-02-04 21:51:08 +0900

    법률 발의 개수 추가

--- src/app/api/assemplyApi.ts
+++ src/app/api/assemplyApi.ts
@@ -25,11 +25,56 @@ interface GetMembersResult {
   nprlapfmaufmqytet: AssemblyData[];
 }
 
+interface Bill {
+  BILL_ID: string;
+  BILL_NO: string;
+  BILL_NAME: string;
+  COMMITTEE?: string;
+  PROPOSE_DT: string;
+  PROC_RESULT?: string;
+  AGE: string;
+  DETAIL_LINK: string;
+  PROPOSER: string;
+  MEMBER_LIST: string;
+  LAW_PROC_DT?: string;
+  LAW_PRESENT_DT?: string;
+  LAW_SUBMIT_DT?: string;
+  CMT_PROC_RESULT_CD?: string;
+  CMT_PROC_DT?: string;
+  CMT_PRESENT_DT?: string;
+  COMMITTEE_DT?: string;
+  PROC_DT?: string;
+  COMMITTEE_ID?: string;
+  PUBL_PROPOSER: string;
+  LAW_PROC_RESULT_CD?: string;
+  RST_PROPOSER: string;
+}
+
+interface Result {
+  CODE: string;
+  MESSAGE: string;
+}
+
+interface Head {
+  list_total_count: number;
+  RESULT: Result;
+}
+
+interface BillData {
+  head: Head;
+  row: Bill[];
+}
+
+interface GetBillsResult {
+  nzmimeepazxkubdpn: BillData[];
+}
+
 const apiKey = process.env.ASSEMBLY_API_KEY;
 const resultType = "json";
 const generation = 21;
 const assembly_api_baseurl = "https://open.assembly.go.kr/portal/openapi/";
 const assembly_members_endpoint = "nprlapfmaufmqytet";
+const search_bills_endpoint = "nzmimeepazxkubdpn";
 
 export const getMembers = async (): Promise<GetMembersResult> => {
   const res = await fetch(
@@ -40,7 +85,51 @@ export const getMembers = async (): Promise<GetMembersResult> => {
       "&Type=" +
       resultType +
       "&DAESU=" +
-      generation
+      generation +
+      "&pSize=" +
+      "350"
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+export const getMember = async (name: string): Promise<GetMembersResult> => {
+  const res = await fetch(
+    assembly_api_baseurl +
+      assembly_members_endpoint +
+      "?KEY=" +
+      apiKey +
+      "&Type=" +
+      resultType +
+      "&DAESU=" +
+      generation +
+      "&NAME=" +
+      name
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+export const searchBills = async (name: string): Promise<GetBillsResult> => {
+  const res = await fetch(
+    assembly_api_baseurl +
+      search_bills_endpoint +
+      "?KEY=" +
+      apiKey +
+      "&Type=" +
+      resultType +
+      "&AGE=" +
+      generation +
+      "&PROPOSER=" +
+      name +
+      "&pSize=" +
+      "350"
   );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
-import { getMembers } from "./api/assemplyApi";
+import { getMembers, searchBills } from "./api/assemplyApi";
 
 export default async function Home() {
   const data = await getMembers();
   const members = data.nprlapfmaufmqytet[1].row;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>국회의원 정보</div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <div>국회의원 법률 발의 정보</div>
       <ul>
         {members.map((member, i) => (
-          <li key={i}>{member.NAME}</li>
+          <li className="flex gap-2" key={i}>
+            <div>{i + 1}</div>
+            <div>{member.NAME}</div>
+            <Billlists name={member.NAME} />
+          </li>
         ))}
       </ul>
     </main>
+  );
+}
+
+async function Billlists({ name }: { name: string }) {
+  const data = await searchBills(name);
+  const bills = data.nzmimeepazxkubdpn[1].row;
+  return (
+    <>
+      <div>{bills.length}</div>
+      {/* <ul>
+        {bills.map((b, i) => (
+          <li key={i}>{b.PROPOSE_DT}</li>
+        ))}
+      </ul> */}
+    </>
   );
 }
